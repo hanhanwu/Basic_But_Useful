333c0272bc7aac371681826aaa49037ea6f1a755
hanhan <wuhanhan999@gmail.com>
2016-09-29 14:25:07 -0700

    update R_basics.R

--- R_basics.R
+++ R_basics.R
@@ -2,3 +2,6 @@
 library(data.table)
 require(data.table)
 setDT(q1)
+
+## NOTE: when R studio has started a new session, all the generated data may still there, but the previous data.table (q1 here) may become
+## data.frame/list again, need to re-run the code above
