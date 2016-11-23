be554cff1b2e30bb343b64209e80b8366eb65f57
hanhan <wuhanhan999@gmail.com>
2016-11-22 18:03:12 -0800

    create mlr_random_forest.R

---
+++ mlr_random_forest.R
@@ -0,0 +1,46 @@
+# This example include what to be noted when useing R Random Forest, especially the one in mlr package
+
+test_train <- train_data
+test_test <- test_data
+ 
+# Newest R Random Forest can no longer handle ordered Factor variables, convert them to numerical data
+fact_cols <- lapply(test_test, is.factor)
+fc <- colnames(subset(test_test, select = fact_cols == T))
+test_test[,(fc) := lapply(.SD, as.numeric), .SDcols = fc]
+summarizeColumns(test_test)
+ 
+fact_cols <- lapply(test_train, is.factor)
+fc <- colnames(subset(test_train, select = fact_cols == T))
+test_train[,(fc) := lapply(.SD, as.numeric), .SDcols = fc]
+summarizeColumns(test_train)
+ 
+# But the label still has to be Factor variable when the predict.type is "response"
+test_test$HasWriteOff <- as.factor(test_test$HasWriteOff)
+test_train$HasWriteOff <- as.factor(test_train$HasWriteOff)
+ 
+train_task <- makeClassifTask(data=data.frame(test_train), target = "HasWriteOff")
+test_task <- makeClassifTask(data=data.frame(test_test), target = "HasWriteOff")
+ 
+getParamSet("classif.randomForest")
+rf_learner <- makeLearner("classif.randomForest", predict.type = "response", par.vals = list(ntree = 200, mtry = 3))
+rf_learner$par.vals <- list(importance = TRUE)
+rf_param <- makeParamSet(
+  makeIntegerParam("ntree",lower = 50, upper = 500),
+  makeIntegerParam("mtry", lower = 3, upper = 10),
+  makeIntegerParam("nodesize", lower = 10, upper = 50)
+)
+ 
+rancontrol <- makeTuneControlRandom(maxit = 50L)
+cv_rf <- makeResampleDesc("CV",iters = 3L)
+rf_tune <- tuneParams(learner = rf_learner, resampling = cv_rf, task = train_task, par.set = rf_param, control = rancontrol, measures = acc)
+rf_tune$x
+rf_tune$y
+rf.tree <- setHyperPars(rf_learner, par.vals = rf_tune$x)
+rf_model <- mlr::train(learner=rf.tree, task=train_task)    # when multiple packages have the same method name, use PackageName::MethodName
+getLearnerModel(rf_model)
+rfpredict <- predict(rf_model, test_task)
+
+# detailed measures to check the accuracy for both Positive and Negative classes
+nb_prediction <- nb_predict$data$response
+dCM <- confusionMatrix(d_test$income_level, nb_prediction)
+dCM
