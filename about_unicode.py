ea8390017452eae2783c416e951c5e22aeb4ae53
hanhan <wuhanhan999@gmail.com>
2017-01-28 18:54:17 -0800

    create about_unicode.py

---
+++ about_unicode.py
@@ -0,0 +1,12 @@
+# There are so many ways to deal with unicode in python, different situation use different methods.
+# I have already tried many methods, however, each time, in different situations, I had to find new solutions again
+# Python version, IDE, data source, etc. will all influence the output
+# so, I's better reocrd my solutions each time here to save my future time
+
+
+# Sample 1: <p>...the police investigations into the women\u2019s disappearances,\u201d Mr. Oppal writes.</p>
+## I etracted the data from HTML directly, when I used str(), I got these characters: \u201d, \u2019
+## This works for me in python 3
+import unidecode
+text = ["<p>"+str(unidecode.unidecode(t.text))+"</p>" for t in content.findAll('p', {"class":''})]
+
