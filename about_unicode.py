7251eb1e6bbbc1a4056dd7edab8c1b527339fdd9
hanhan <wuhanhan999@gmail.com>
2017-03-20 22:31:00 -0700

    update about_unicode.py

--- about_unicode.py
+++ about_unicode.py
@@ -14,3 +14,16 @@ text = ["<p>"+str(unidecode.unidecode(t.text))+"</p>" for t in content.findAll('
 # Sample 2: could\xe2\x80\x99t  (means couldn't)
 import unidecode
 word_lst = [str(unidecode.unidecode(w.decode('utf-8'))) for w in text.split()]
+
+
+# Sample 3: the error message could be: UnicodeDecodeError: 'utf-8' codec cannot decode byte 0x93...
+## this is a more complex situation, especially when you are using python3.*
+# first of all you have to read the data from file as 'rb' and decode each line
+text_string = ""
+with open('input_file_path', 'rb') as indata:
+  for l in indata:
+    text_string = " ".join([text_string, l.strip().decode('utf-8', 'backlashescape')])
+    
+# BUT! Here, the python version could be different, check the multiple solutions here to cater for different situations:
+# answer by "anatoly techtonik"
+# http://stackoverflow.com/questions/606191/convert-bytes-to-a-python-string
