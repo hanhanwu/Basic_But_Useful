714371e9e947f7643dfc40df31574ca16044b735
hanhan <wuhanhan999@gmail.com>
2017-04-28 19:36:19 -0700

    create python_dct_and_order.py

---
+++ python_dct_and_order.py
@@ -0,0 +1,20 @@
+# Python dictionary is randomly ordered
+# 1. arrange keys in a dictionary in order
+# 2. make a dictionary arranged in key order
+
+
+# 1. arrange keys in a dictionary in order
+
+from collections import OrderedDict
+rich_dct[file_name] = OrderedDict([("Text Length", 0), ("Nouns", {"List":[], "Average SO":0}),
+                                                   ("Verbs", {"List":[], "Average SO":0}),
+                                                   ("Adjectives", {"List":[], "Average SO":0}),
+                                                   ("Adverbs", {"List":[], "Average SO":0}), ("SO by Sentence",[]),
+                                                   ("Total SO", 0)])
+# Then just assign value to rich_dct like normal dictionary
+
+
+# 2. make a dictionary arranged in key order
+sorted_dict = OrderedDict(sorted(rich_dct.items(), key=lambda t: t[0]))
+with open(richout_output, 'w') as richout:
+    json.dump(sorted_dict, richout)
