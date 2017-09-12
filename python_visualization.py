ac667a3988ed113f0f4bd3c84779945368a4dc9c
hanhan <hanhanwu@users.noreply.github.com>
2017-09-12 15:46:31 -0700

    create python_visualization.py

---
+++ python_visualization.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_values = ['A', 'B', 'C', 'D']
+x_pos = np.arange(len(x_values))
+y_values = [10, 20, 30, 40]
+plt.bar(x_pos, y_values, align='center', alpha=0.5)
+plt.xticks(x_pos, x_values)
+plt.ylabel('Y')
+plt.title('Edit Your Title')
+plt.show()
