0b389ab7634a26ef741457d23bc2a4baa74ff536
hanhan <hanhanwu@users.noreply.github.com>
2017-10-10 10:52:46 -0700

    update python_excel.py

--- python_excel.py
+++ python_excel.py
@@ -7,4 +7,4 @@ wb = openpyxl.load_workbook(my_excel)  # read excel file
 my_sheet = wb.get_sheet_by_name(sheet_name) # read sheet
 col = my_sheet['A4':'A10']  # read multiple rows in a col, here 'A', 'B'.. indicates cols in Excel
 for elem in col:
-    print elem.value # access to the cell value
+    print elem[0].value # access to the cell value
