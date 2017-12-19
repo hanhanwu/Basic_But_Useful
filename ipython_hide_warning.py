6c18f913c8b613e80b7f5d6695aefac5c1ca60b7
hanhan <hanhanwu@users.noreply.github.com>
2017-12-18 20:34:09 -0800

    create ipython_hide_warning.py

---
+++ ipython_hide_warning.py
@@ -0,0 +1,17 @@
+from IPython.display import HTML
+
+# hide Ipython warnings, you need to click "here" at the bottom
+
+HTML('''<script>
+code_show_err=false; 
+function code_toggle_err() {
+ if (code_show_err){
+ $('div.output_stderr').hide();
+ } else {
+ $('div.output_stderr').show();
+ }
+ code_show_err = !code_show_err
+} 
+$( document ).ready(code_toggle_err);
+</script>
+To toggle on/off output_stderr, click <a href="javascript:code_toggle_err()">here</a>.''')
