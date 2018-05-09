d6a58be67a6ddc4da98854e648da9950300dd404
hanhan <hanhanwu@users.noreply.github.com>
2018-05-09 15:52:50 -0700

    update basics

--- python2psql.py
+++ python2psql.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-
+# Connect to Redshift and run query
 # Get credentials for database access
 read_user_name = "[USERNAME]"
 read_passwd = "[PSWD]"
@@ -20,3 +20,9 @@ test = pd.read_sql(query, engine, index_col=None, coerce_float=True, params=None
                       parse_dates=None, columns=None, chunksize=None)
 
 print test.T.to_dict().values()
+
+
+
+# truncate float without rounding
+int(s* 100)/100.0  # keep 2 digits
+int(s* 1000)/1000.0  # keep 3 digits
