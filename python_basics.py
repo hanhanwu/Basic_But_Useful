78bd92edb2bb750cf966104f85dcf28170b3775f
hanhan <hanhanwu@users.noreply.github.com>
2018-06-11 22:02:08 -0700

    update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -4,3 +4,12 @@ warnings.filterwarnings("ignore")
 
 # find common elements in multiple lists
 set.intersection(*[set(list) for list in p])
+
+# temporary list append elements in iteration
+## I just found something weird with python, that is even if in each iteration, 
+## I assigned the original list to the temporary list, and then tried to append new elements in this temporary list,
+## its values will be brought to the next iteration. I have to initialize this temporary as empty in each iteration, like this
+for i in range(10):
+  tmp_lst = []
+  tmp_lst.append(elem)
+  tmp_lst.extend(origin_lst)
