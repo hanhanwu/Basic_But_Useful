f699ced58c53e9faebdd0e333b15864de914ae97
hanhan <hanhanwu@users.noreply.github.com>
2019-03-07 23:55:56 -0800

    ec2 & REDSHIFT

--- python2psql.py
+++ python2psql.py
@@ -1,6 +1,32 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+# Better way for DB connection
+## this can be used for not only Redshift data reading, but also shcema creation, AWS copy command, etc.
+def read_rs_data(query, credential_file, db, has_return_results):
+    """
+    Connect to the specified database and execute the query.
+    :param query: The query you need to execute
+    :param db: choose a db used as the key in crednetials file, so that you can load credentials for that DB
+    :return: Query results as a dataframe
+    """
+    username, pwd, host, database, port = generate_db_conn(credential_file, db)
+    
+    if has_return_results:
+        conn = 'postgresql://' + username + ':' + pwd + '@' + host + ':' + port + '/' + database
+        print(conn)
+        engine = create_engine(conn)
+        df = pd.read_sql(query, engine, coerce_float=True, params=None,
+                      parse_dates=None, columns=None, chunksize=None)
+        return df
+    else:
+        conn_string = "dbname=%s host=%s port=%s user=%s password=%s" % (database, host, port, username, pwd)
+        with psycopg2.connect(conn_string) as conn:
+            with conn.cursor() as curs:
+                curs.execute(query)
+                curs.close()
+            conn.commit()
+
 # Connect to Redshift and run query
 # Get credentials for database access
 read_user_name = "[USERNAME]"
