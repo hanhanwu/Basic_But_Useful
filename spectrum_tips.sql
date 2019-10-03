e7da34b0f5f74d6b21665ab9d2db832ecc24cd66
hanhan <hanhanwu@users.noreply.github.com>
2019-10-02 17:00:10 -0700

    spectrum notes

--- spectrum_tips.sql
+++ spectrum_tips.sql
@@ -1,2 +1,5 @@
 # How to run faster
 ## use date as the filtering condition. This works even you have already used datetime in the filtering condition.
+## spectrum cost is calculated by how much space you have used, so better to use enough filtering criteria. 
+### But LIMIT clause won't save any money
+## aggregate functions are cheap
