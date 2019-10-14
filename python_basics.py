4035dafeeae61fb8e5eda8b45770c274fd0082fd
hanhan <hanhanwu@users.noreply.github.com>
2019-10-13 17:26:34 -0700

    python basics

--- python_basics.py
+++ python_basics.py
@@ -33,3 +33,14 @@ lst = [elem.strip() for elem in re.split('[()]',ua_string)]
 lst
 # Output:
 ['Mozilla/5.0', 'Macintosh; Intel Mac OS X 10_9_4', 'AppleWebKit/537.36', 'KHTML, like Gecko', 'Chrome/41.0.2272.104 Safari/537.36']
+
+
+# read and show plot color image in IPython
+%pylab inline
+import cv2
+from matplotlib import pyplot as plt
+img = cv2.imread('test_plot.png', cv2.IMREAD_COLOR)
+plt.figure(figsize = (10,10))
+pylab.imshow(img)
+pylab.axis('off')
+pylab.show()
