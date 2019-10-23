57c22046ea96dc94160e66abd1e17eef62d91d59
hanhan <hanhanwu@users.noreply.github.com>
2019-10-23 11:03:55 -0700

    Update python_dataframe.py

--- python_dataframe.py
+++ python_dataframe.py
@@ -46,6 +46,8 @@ df['days_diff'] = (df['max_time'] - df['min_time']).dt.days
 # extract date from datetime string
 ## check those symbols: https://docs.python.org/2/library/datetime.html
 my_date = datetime.datetime.strptime(my_datetime_str, "%Y-%m-%d %H:%M:%S").date()
+## extract year_month
+df['month'] = df['my_datetime'].apply(lambda v: v.to_period('M'))
 
 # count duplicated rows
 df.duplicated().sum()
