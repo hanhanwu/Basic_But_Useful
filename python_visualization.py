31d0c67980a482233b868ed578f7ac1ebed66140
hanhan <hanhanwu@users.noreply.github.com>
2020-12-03 18:38:23 -0800

    xticks rotation

--- python_visualization.py
+++ python_visualization.py
@@ -17,7 +17,7 @@ x_values = ['A', 'B', 'C', 'D']
 x_pos = np.arange(len(x_values))
 y_values = [10, 20, 30, 40]
 plt.bar(x_pos, y_values, align='center', alpha=0.5)
-plt.xticks(x_pos, x_values)
+plt.xticks(x_pos, x_values, rotation='30', fontsize=14, horizontalalignment="right")
 plt.ylabel('Y')
 plt.title('Edit Your Title')
 plt.show()
